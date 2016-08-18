ba6f5d775cc414f16cc1994241b25df29fb790b0
robertka <robertka@ROBERTKA01>
2016-08-18 10:40:54 +0200

    Initial version.

---
+++ Src/EmbeddedResourceProvider.cs
@@ -0,0 +1,43 @@
+﻿using Docati.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docati.Api.Demo
+{
+    public class EmbeddedResourceProvider : ResourceProvider
+    {
+        private readonly string asmPath;
+
+        public EmbeddedResourceProvider()
+            : base(new Uri(@"emb://", UriKind.Absolute), true) // BaseUri is set to 'emb://Docati.Api.Demo/'
+        {
+            asmPath = Assembly.GetExecutingAssembly().GetName().Name;
+        }
+
+        public override ResourceStream GetResource(Uri resourceUri)
+        {
+            var isEmbedded = !resourceUri.IsAbsoluteUri
+                || resourceUri.AbsoluteUri.StartsWith(this.BaseUri.AbsoluteUri, StringComparison.OrdinalIgnoreCase);
+            if (isEmbedded)
+            {
+                string embeddedResourceName = !resourceUri.IsAbsoluteUri
+                    ? resourceUri.OriginalString
+                    : resourceUri.PathAndQuery;
+
+                string fullEmbeddedResourceName = asmPath + "." + embeddedResourceName;
+                var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fullEmbeddedResourceName);
+                if (stream == null)
+                    throw new FileNotFoundException("The embedded resource does not exist", fullEmbeddedResourceName);
+
+                return base.GetResource(resourceUri, stream);
+            }
+            else
+                return base.GetResource(resourceUri);
+        }
+    }
+}
---
+++ Src/Program.cs
@@ -0,0 +1,38 @@
+﻿using Docati.Api;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Docati.Api.Demo
+{
+    // If this does not compile, you may need to remove and add the reference to Docati.Api.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The license is included in this project as an embedded resource (build-action). Make sure you replace it with your own.
+            // It only needs to be applied once in the app-domain, eg in a static constructor or inside an owin-startup class.
+            //License.ApplyLicense(Assembly.GetExecutingAssembly().GetManifestResourceStream("Docati.Api.Demo.License.lic"));
+
+            // A custom resource provider is used, to be able to load templates (and whatever they need) from embedded resources.
+            // The standard ResoureProvider supports loading from disk and web/http addresses only.
+            var resourceProvider = new EmbeddedResourceProvider();
+
+            // A memorystream is defined to hold the final document
+            MemoryStream doc = new MemoryStream();
+
+            // Invoke the generator (the using-statements are required to make sure all internal streams etc are disposed.
+            // Although this code generates a single document, the created DocBuilder can be reused to create multiple documents.
+            using (var builder = new DocBuilder("emb://Template.docx", resourceProvider))
+            {
+                using (Stream data = Assembly.GetExecutingAssembly().GetManifestResourceStream("Docati.Api.Demo.data.xml")) // Just like the license file, the data file is loaded from embedded resource as well
+                {
+                    builder.Build(data, DataFormat.Xml, (Stream)doc, null, DocumentFileFormat.SameAsTemplate);
+                }
+            }
+
+            // doc now contains the final document, so let's write it to disk
+            File.WriteAllBytes("MyTemplate-Result.docx", doc.ToArray());
+        }
+    }
+}

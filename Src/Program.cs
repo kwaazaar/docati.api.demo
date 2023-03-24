bedba52ebda07c67b1b0f1663878e493d3ce2050
Robert te Kaat <robert.te.kaat@gmail.com>
2023-03-24 10:14:50 +0100

    Package updates

--- Src/Program.cs
+++ Src/Program.cs
@@ -11,7 +11,7 @@ namespace Docati.Api.Demo
         async static Task Main(string[] args)
         {
             /*
-            // The license is included in this project is an embedded resource (build-action). Make sure you replace it with your own.
+            // The license is included in this project is an embedded resource. Make sure you replace it with your own.
             // NB: We sometimes replace it with a temporary trial license, so you may want to see if it just works.
             // The license only needs to be applied once in the app-domain, eg in a static constructor or inside an owin-startup class.
             using (var licenseStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Docati.Api.Demo.License.lic"))
